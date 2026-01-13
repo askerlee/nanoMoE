42453246c5bb274f40e7883e9733ef4b4e27df39
askerlee <shaohua@gmail.com>
2026-01-13 15:19:41 +0800

    Change default block_size to 1024

--- config/train_nano_moe.py
+++ config/train_nano_moe.py
@@ -57,7 +57,7 @@ n_embd = 512
 # these make the total batch size be ~0.5M
 # 12 batch size * 1024 block size * 5 gradaccum * 8 GPUs = 491,520
 batch_size = 64
-block_size = 512
+block_size = 1024
 gradient_accumulation_steps = 2
 
 # epoch-based training
