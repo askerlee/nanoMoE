baef84f060be3df0782d4c24eab4b25a895afafa
askerlee <shaohua@gmail.com>
2026-01-21 13:23:36 +0800

    Reduce default train_capacity to 1

--- config/train_nano_moe.py
+++ config/train_nano_moe.py
@@ -39,8 +39,7 @@ gate_output_loss_weight = 0.0001
 gate_diversity_loss_weight = 0.01
 
 use_noisy_top_k = False
-# Intentionally use a small capacity to show the advantage of the router ortho loss.
-train_capacity = 1.25
+train_capacity = 1
 eval_capacity = 3.0
 use_switch_tfm_init = True
 router_use_full_prec = True
