8989b4614f57a00a7d127ac8f1c13b076749b770
askerlee <shaohua@gmail.com>
2026-01-21 14:50:36 +0800

    Allow passing datasets as a string

--- train.py
+++ train.py
@@ -238,6 +238,9 @@ device_type = 'cuda' if 'cuda' in device else 'cpu' # for later use in torch.aut
 ptdtype = {'float32': torch.float32, 'bfloat16': torch.bfloat16, 'float16': torch.float16}[dtype]
 ctx = nullcontext() if device_type == 'cpu' else torch.amp.autocast(device_type=device_type, dtype=ptdtype)
 
+if isinstance(datasets, str):
+    datasets = [datasets]
+    
 train_datasets = []
 val_datasets = []
 
